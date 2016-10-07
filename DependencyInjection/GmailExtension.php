23e7ff248e973e4531231db861568b57d278fbd9
Luis Galeas <galeaspablo@hotmail.com>
2016-10-07 17:29:40 +0100

    rename

--- DependencyInjection/GmailExtension.php
+++ DependencyInjection/GmailExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class FLGmailExtension extends Extension
+class GmailExtension extends Extension
 {
     /**
      * {@inheritdoc}
