510bdf1de1cdbbca4f5e853f5b61e3b48e089142
Luis Galeas <galeaspablo@hotmail.com>
2016-10-07 18:00:20 +0100

    correct extension

--- DependencyInjection/FLGmailExtension.php
+++ DependencyInjection/FLGmailExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class GmailExtension extends Extension
+class FLGmailExtension extends Extension
 {
     /**
      * {@inheritdoc}
